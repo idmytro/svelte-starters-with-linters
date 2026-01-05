087951a67085d479f19b95632a28ecc5eca051ec
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2026-01-05 19:46:03 +0100

    build: cases

--- starters/sv-svelte5-prettier-eslint-npm-advanced-lts/.prettierrc.mjs
+++ starters/sv-svelte5-prettier-eslint-npm-advanced-lts/.prettierrc.mjs
@@ -3,7 +3,7 @@ export default {
 	useTabs: true,
 	singleQuote: true,
 	trailingComma: 'none',
-	printWidth: 100,
+	// printWidth: 100,
 	plugins: ['prettier-plugin-svelte'],
 	overrides: [
 		{
--- starters/sv-svelte5-prettier-eslint-npm-advanced-lts/src/routes/+page.svelte
+++ starters/sv-svelte5-prettier-eslint-npm-advanced-lts/src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <!-- +page.svelte -->
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation documentation documentation
+</p>
