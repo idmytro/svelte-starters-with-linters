bcf4c240f522874a09fa8748f1df88972b4bc4c3
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2026-01-05 23:16:34 +0100

    build: new stylistic

--- starters/sv-svelte5-eslint-npm-stylistic/eslint.config.js
+++ starters/sv-svelte5-eslint-npm-stylistic/eslint.config.js
@@ -1,4 +1,3 @@
-import prettier from 'eslint-config-prettier';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
@@ -15,15 +14,13 @@ export default defineConfig(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			"no-undef": 'off'
 		}
 	},
 	{
--- starters/sv-svelte5-prettier-eslint-npm-stylistic/README.md
+++
@@ -1,7 +0,0 @@
-# svelte5-prettier-eslint-npm-official
-
-```bash
-npx sv create --template minimal --types ts --add prettier eslint --install npm .
-```
-
-version 2026.05.01
