d51019c1959c1c8bc14009a7f38aad96a8f16974
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2026-01-05 19:53:06 +0100

    build: vite-svelte5-ts-npm-sv-prettier-js-lorem

---
+++ starters/vite-svelte5-ts-npm-sv-prettier-js-lorem/.prettierrc.mjs
@@ -0,0 +1,16 @@
+// .prettierrc.mjs
+export default {
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	// printWidth: 100,
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte'
+			}
+		}
+	]
+};
